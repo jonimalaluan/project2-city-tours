34d6c9449047cd87ca0c4499ae264e37b20ab990
jonithereactdev <jonithereactdev@gmail.com>
2019-06-26 22:29:25 +0800

    displayed all tours

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -5,30 +5,25 @@ class Tour extends Component {
   //   state = {};
 
   render() {
+    const { city, img, name, info } = this.props.tour;
     return (
       <article className="tour">
         <div className="img-container">
-          <img
-            src="https://images.pexels.com/photos/2440013/pexels-photo-2440013.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            alt="/"
-          />
+          <img src={img} alt="/" />
           <span className="close-btn">
             <i className="fas fa-window-close" />
           </span>
         </div>
         <div className="tour-info">
-          <h3>city</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
-            info
+            info{" "}
             <span>
               <i className="fas fa-caret-square-down" />
             </span>
           </h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam,
-            molestiae officiis asperiores nisi ad alias!
-          </p>
+          <p>{info}</p>
         </div>
       </article>
     );
